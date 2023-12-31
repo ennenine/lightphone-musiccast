ac804efa0ebaaf22fa6f29437b4292e9563ac2ff
Nate Swart <839888+nateswart@users.noreply.github.com>
2023-12-30 20:42:25 -0500

    Move config variables to settings.php

--- php/index.php
+++ php/index.php
@@ -1,27 +1,7 @@
 <?php
 
-// ------------------------------------------------------------------------
-// START CONFIG
+include 'settings.php';
 
-// Base URL of this service, include trailing slash
-$baseurl = "https://yourdomain.com/";
-
-// Date to start the playlist publishing from. 
-// Setting it in the past ensures other (actual) podcast episodes appear first on the Light Phone
-$publish_date = date_format( date_create('1980-01-01'), 'D, d M Y H:i:s O' ); 
-
-// Location of playlists as mapped in docker container, including trailing slash
-$playlist_root = '/playlists/';
-
-// Full path for media mount, including trailing slash. 
-// Be sure this matches what Jellyfin's docker and this docker have set.
-$media_root = '/media/Music/';
-
-// If you want your playlist randomized (every time Light Phone refreshes the podcast feed)
-$randomize = true;
-
-// END CONFIG
-// ------------------------------------------------------------------------
 	
 if(isset($_GET['playlist'])) {
 	$playlist = urldecode($_GET['playlist']);
@@ -136,8 +116,8 @@ function processPlaylist($playlist_name) {
 
 
         // Last Build Date
-		$last_build_node = $dom->createElement('lastBuildDate');
-		$last_build_node->appendChild(
+	$last_build_node = $dom->createElement('lastBuildDate');
+	$last_build_node->appendChild(
 	 	$dom->createTextNode( formatDateForRSS( date_create('now') ) ) );
         $channel_node->appendChild($last_build_node);
 
@@ -154,7 +134,7 @@ function processPlaylist($playlist_name) {
 
 		// Item Publish Date
 		$pub_node = $dom->createElement('pubDate');
-		$pub_node->appendChild( $dom->createTextNode( formatDateForRSS($publish_date) ) );
+			$pub_node->appendChild( $dom->createTextNode( formatDateForRSS($publish_date) ) );
 		$item_node->appendChild( $pub_node ); 
 
 		// Item GUID
@@ -252,6 +232,3 @@ function streamSong($song) {
 	fclose($fp);
 	exit();
 }
-
-
-?>
